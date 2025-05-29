819de0afe4012ed308e5352db5e4eb9334892792
Minsu9 <pms441079@gmail.com>
2025-05-29 17:59:03 +0900

    25.05.29

--- 03_JavaScript/실습07_회원가입.js
+++ 03_JavaScript/실습07_회원가입.js
@@ -1,3 +1,4 @@
+const input = document.querySelector("#input");
 idg.addEventListener("input", (e) => {
   console.log(e.target.value);
 });
@@ -16,3 +17,9 @@ email.addEventListener("input", (e) => {
 phoneN.addEventListener("input", (e) => {
   console.log(e.target.value);
 });
+
+//  순서대로 어떻게 써야 하는가 ?
+// 1. 정규표현식  test 함수가지고 true / false <- 조건문!
+// 2. 조건문으로 true인경우 if문에서 P태그 내에 내가 원하는 걸 처리
+// 3. 텍스트 변경.. (textContent, innerHTML), 색깔을 변경 .. style.color =
+// 4. else  <-- false인 경우 변하는 텍스트 + 빨간 텍스트 색깔
